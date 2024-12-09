9c94fa3322ddd7d204b91293d9222b61b2c690f5
ArsiniiPakhomov <165683628+ArsiniiPakhomov@users.noreply.github.com>
2024-12-09 19:05:49 +0300

    Add files via upload

--- news_portal/news_portal/settings.py
+++ news_portal/news_portal/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
+    'sign',
+    'protect',
     'django_filters',
     'django.contrib.flatpages',
-    'django.contrib.sites'
+    'django.contrib.sites',
+    
 ]
 SITE_ID = 1
 
@@ -123,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = 'sign/login/'
+LOGIN_REDIRECT_URL = '/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
--- news_portal/news_portal/urls.py
+++ news_portal/news_portal/urls.py
@@ -21,7 +21,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('pages/', include('django.contrib.flatpages.urls')),
-     path('posts/', include('news.urls')),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('posts/', include('news.urls')),
+    path('', include('protect.urls')),
+    path('sign/', include('sign.urls'))
      
 ]
